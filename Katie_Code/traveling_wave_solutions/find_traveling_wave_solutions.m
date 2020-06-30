2e36f23369c6b9942dc05aa892380ea183587a35
Katie Oliveras <oliveras@seattleu.edu>
2020-06-30 12:45:33 -0700

    Added comments to code

--- Katie_Code/traveling_wave_solutions/find_traveling_wave_solutions.m
+++ Katie_Code/traveling_wave_solutions/find_traveling_wave_solutions.m
@@ -4,41 +4,64 @@ clc
 
 format long
 
+
+% - - - - - - - - - - - - - - - - - - - 
+% Model Parameters
+% - - - - - - - - - - - - - - - - - - - 
 Nx = 100;                       % number of x points
 x = linspace(0,2*pi,Nx);        % x grid points
 dx = x(2) - x(1);               % determine grid spacing
 L = x(end) - x(1);              % determine the period
+h = 0.5;                        % depth of the fluid
+g = 1;                          % gravity
 
-% --------------------
+% - - - - - - - - - - - - - - - - - - - 
 % Fourier Parameters
+% - - - - - - - - - - - - - - - - - - - 
 Nn = 10;                        % number of Fourier Modes
 N = (-Nn:Nn)';                  % a vector of modes
 
 
-% ---------------------
+
+% - - - - - - - - - - - - - - - - - - - 
 % Initial Guess for Newton Method
+% - - - - - - - - - - - - - - - - - - - 
 iniGuessEta = .001*cos(x);
 iniGuessEtaHat = hat(iniGuessEta,x,N);
-iniGuessC = sqrt(tanh(0.5));
+iniGuessC = sqrt(g*tanh(h));
 iniGuessX = [iniGuessEtaHat;iniGuessC];
 
 
+% - - - - - - - - - - - - - - - - - - - 
+% Use the internal Matlab "Newton's Method
+% - - - - - - - - - - - - - - - - - - - 
 options = optimset('TolFun',1e-12,'TolX',1e-12,'jacobian','on','display','iter');
 X = fsolve(@(X) solveFunct(X,x,N), iniGuessX,options);
-%X = newtonsMethod(iniGuessX,x,N);
 
+
+% - - - - - - - - - - - - - - - - - - - 
+% Retreive and plot results
+% - - - - - - - - - - - - - - - - - - - 
 etaHat = X(1:end-1);
 eta = invHat(etaHat,x,N);
-plot(x,iniGuessEta)
+plot(x,iniGuessEta,'.-')
 hold on
 plot(x,eta)
+axis tight
 
 
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Functions
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% - - - - - - - - - - - - - - - - - - - 
+% Compute the Fourier Coefficients
+% - - - - - - - - - - - - - - - - - - - 
 function uHat = hat(u,x,N)
 dx = x(2)-x(1);
 L = x(end)-x(1);
@@ -46,18 +69,32 @@ intTerm = exp(-1i*N*x).*u;      % create a matrix mesh of the Fourier integrand
 uHat = sum(intTerm(:,2:end),2)*dx/L';   % This is the trapz. rule for periodic functions
 end
 
+% - - - - - - - - - - - - - - - - - - - 
+% Compute the Fourier Series from Fourier Coefficients
+% - - - - - - - - - - - - - - - - - - - 
 function u = invHat(uHat,x,N)
 sumTerm = exp(1i*N*x).*uHat;
 u = sum(sumTerm,1);
+if norm(imag(u))<1e-10
+    u = real(u);
+else
+    disp('Complex function')
+end
 end
 
+
+% - - - - - - - - - - - - - - - - - - - 
+% Compute Derivative Spectrally
+% - - - - - - - - - - - - - - - - - - - 
 function uX = d(u,x,N)
 uHat = hat(u,x,N);
 uXHat = uHat.*(1i*N);
 uX = invHat(uXHat,x,N);
 end
 
-
+% - - - - - - - - - - - - - - - - - - - 
+% Compute the Nonlinear Eqns & Jacobian
+% - - - - - - - - - - - - - - - - - - - 
 function [F dF] = solveFunct(X,x,N);
 Nn = max(N);
 etaHat = X(1:end-1);
@@ -83,6 +120,10 @@ F(2*Nn+2) = sum(etaHat)-.001;
 dF = jacobian(X,x,N);
 end
 
+
+% - - - - - - - - - - - - - - - - - - - 
+% Calculate the Jacobian
+% - - - - - - - - - - - - - - - - - - - 
 function dF = jacobian(X,x,N)
 
 Nn = max(N);
@@ -115,23 +156,5 @@ end
 dF(2*Nn+2,1:2*Nn+1) = ones(1,2*Nn+1);
 
 end
-
-
-function X = newtonsMethod(X,x,N);
-
-iterCount = 0;
-normError = 1;
-[F dF] = solveFunct(X,x,N);
-size(F)
-size(dF)
-while (normError>1e-8)&&(iterCount<100)
-    X = X - inv(dF')*F;
-    [F dF] = solveFunct(X,x,N);
-    normError = norm(F);
-    iterCount = iterCount + 1;
-end
-disp(['Error is ',num2str(normError),' calculated in ',num2str(iterCount),' iterations'])
-end
-
 
 
